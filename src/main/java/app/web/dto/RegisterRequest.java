b9e5a3c5ce21633e510046e8492ac65ad71cd367
Momchil Tsanov <momchil395@gmail.com>
2025-11-07 23:00:12 +0200

    set regex for password

--- src/main/java/app/web/dto/RegisterRequest.java
+++ src/main/java/app/web/dto/RegisterRequest.java
@@ -3,6 +3,7 @@ package app.web.dto;
 import app.user.model.Country;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 
@@ -16,9 +17,13 @@ public class RegisterRequest {
     @NotBlank
     @Size(min = 6, max = 26, message = "Username length must be between 6 and 26 symbols.")
     private String username;
-    @NotBlank
-    @Size(min = 6, max = 6, message = "Password must be exactly 6 symbols.")
+
+    @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>/?])(?=.*[a-zA-Z]).{8,}$",
+             message = "Password must contain at least 1 uppercase letter, 1 digit, 1 special character, and be at least 8 characters long"
+    )
     private String password;
+
     @NotNull
     private Country country;
 }
