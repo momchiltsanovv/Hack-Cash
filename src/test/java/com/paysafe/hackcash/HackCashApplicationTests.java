36120f5e3a18a45b870a81c8e9a87ceb271f0cb8
Momchil Tsanov <momchil395@gmail.com>
2025-11-07 15:35:54 +0200

    structure

---
+++ src/test/java/com/paysafe/hackcash/HackCashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paysafe.hackcash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackCashApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
