7390fb9a20b4605ee96fabdb38b30a7cfcc31989
Momchil Tsanov <momchil395@gmail.com>
2025-11-08 23:05:54 +0200

    notification functionallity done

--- src/main/java/app/web/NotificationController.java
+++ src/main/java/app/web/NotificationController.java
@@ -1,6 +1,7 @@
 package app.web;
 
 import app.notification.client.dto.Email;
+import app.notification.client.dto.PreferenceResponse;
 import app.notification.service.NotificationService;
 import app.security.UserData;
 import app.utils.EmailUtils;
@@ -8,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Collections;
 import java.util.List;
 
 @Controller
@@ -40,6 +43,33 @@ public class NotificationController {
         modelAndView.addObject("nonFailedEmailsCount", EmailUtils.getNonFailedEmailsCount(userEmails));
         modelAndView.addObject("failedEmailsCount", EmailUtils.getFailedEmailsCount(userEmails));
 
+        return modelAndView;
+    }
+
+    @PostMapping("/enable")
+    public ModelAndView enableNotifications(@AuthenticationPrincipal UserData user) {
+        PreferenceResponse currentPreference = notificationService.getPreferenceByUserId(user.getUserId());
+        notificationService.upsertPreference(user.getUserId(), true, currentPreference.getContactInfo());
+        return new ModelAndView("redirect:/notifications");
+    }
+
+    @PostMapping("/disable")
+    public ModelAndView disableNotifications(@AuthenticationPrincipal UserData user) {
+        PreferenceResponse currentPreference = notificationService.getPreferenceByUserId(user.getUserId());
+        notificationService.upsertPreference(user.getUserId(), false, currentPreference.getContactInfo());
+        return new ModelAndView("redirect:/notifications");
+    }
+
+    @PostMapping("/clear")
+    public ModelAndView clearHistory(@AuthenticationPrincipal UserData user) {
+        ModelAndView modelAndView = new ModelAndView("notifications");
+        
+        // Return empty list to clear notifications from screen
+        modelAndView.addObject("preference", notificationService.getPreferenceByUserId(user.getUserId()));
+        modelAndView.addObject("lastEmails", Collections.emptyList());
+        modelAndView.addObject("nonFailedEmailsCount", 0L);
+        modelAndView.addObject("failedEmailsCount", 0L);
+        
         return modelAndView;
     }
 }
